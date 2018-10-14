7ef84b905c1759d0f7210a027a14cb652edebff8
Jacob Baird <jacobbaird@Jacobs-MacBook-Pro.local>
2018-10-14 00:42:06 -0500

    Added the button for toggling the chat bot, and added it to the details page and welcome page

--- WelcomeU/src/components/chatBot.js
+++ WelcomeU/src/components/chatBot.js
@@ -2,12 +2,25 @@ import React from 'react';
 import Iframe from 'react-iframe';
 
 class chatBot extends React.Component {
+
+  state = { showIframe: false };
+
+  toggleIframe = () => {
+    this.setState({
+      showIframe : !this.state.showIframe
+    });
+  }
   render() {
-    return (<Iframe
+    if(!this.state.showIframe){
+      return (<button onClick={this.toggleIframe}>Chat Bot</button>);
+    }
+    return (<div><button onClick={this.toggleIframe}>Chat Bot</button>
+    <Iframe
     allow="microphone"
     width="350"
     height="430"
-    url="https://console.dialogflow.com/api-client/demo/embedded/1b2f5acd-ce46-4cfb-9809-1aa29c9f872a" />)
+    url="https://console.dialogflow.com/api-client/demo/embedded/1b2f5acd-ce46-4cfb-9809-1aa29c9f872a" />
+    </div>)
   }
 }
 
