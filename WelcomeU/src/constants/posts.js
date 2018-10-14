343dc48ad55e93eb0d4aacd769baf59d5dc6ba26
zjyw <wjyzhang@gmail.com>
2018-10-14 01:43:31 -0500

    add posts

---
+++ WelcomeU/src/constants/posts.js
@@ -0,0 +1,21 @@
+export const EmploymentTasks = [
+    {message:'Search for relevant jobs', url:'https://fierce-castle-49474.herokuapp.com/category/15/job-listings'},
+    {message:'Practice interview techniques', url:'https://fierce-castle-49474.herokuapp.com/topic/72/7-step-job-interview-preparation'},
+    {message:'Learn employee rights & obligations', url:'https://fierce-castle-49474.herokuapp.com/topic/78/employee-rights-in-missouri'},
+];
+
+export const ChildcareTasks = [
+    {message:'Find your school district', url:'https://fierce-castle-49474.herokuapp.com/topic/69/find-nearby-schools-and-learn-your-school-district'}
+];
+
+export const HealthcareTasks = [
+    {message:'Complete a health assessment', url:'https://fierce-castle-49474.herokuapp.com/topic/73/healthcare-assessment'},
+    {message:'Obtain insurance', url:'https://fierce-castle-49474.herokuapp.com/topic/74/understanding-health-insurance-through-your-employer'},
+    {message:'Find a healthcare provider', url:'https://fierce-castle-49474.herokuapp.com/topic/75/finding-a-healthcare-provider'}
+];
+
+export const FinancialPlanningTasks = [
+    {message:'Establish a checking account', url:'https://fierce-castle-49474.herokuapp.com/topic/77/opening-a-bank-account'}
+];
+
+export default EmploymentTasks;
